78f3eab88ffdda59ec031bdac636e74f8082f869
marthinsalakory <marthinsalakory11@gmail.com>
2026-06-26 18:36:43 +0900

    Update contact phone number in Footer component for accuracy.

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -243,7 +243,7 @@ export default function Footer({ profile }: FooterProps) {
                     d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z"
                   />
                 </svg>
-                <span className="text-gray-400">+62 123 456 7890</span>
+                <span className="text-gray-400">+62 813 1881 2027</span>
               </li>
               <li className="flex items-start gap-2">
                 <svg
